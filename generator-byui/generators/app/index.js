4aa2893e444a8ddae3bffa7804bd3eefa9d79abc
Gewondjan <jedimasterryan@byui.edu>
2018-11-28 17:57:33 -0700

    figured out where the file goes with a test

--- generator-byui/generators/app/index.js
+++ generator-byui/generators/app/index.js
@@ -4,33 +4,35 @@ const chalk = require('chalk');
 const yosay = require('yosay');
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the amazing ${chalk.yellow('project document')} generator!`)
-    );
+  // prompting() {
+  //   // Have Yeoman greet the user.
+  //   this.log(
+  //     yosay(`Welcome to the amazing ${chalk.yellow('project document')} generator!`)
+  //   );
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+  //   const prompts = [{
+  //     type: 'confirm',
+  //     name: 'someAnswer',
+  //     message: 'Would you like to enable this option?',
+  //     default: true
+  //   }];
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
+  //   return this.prompt(prompts).then(props => {
+  //     // To access props later use this.props.someAnswer;
+  //     this.props = props;
+  //   });
+  // }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    this.fs.copyTpl(
+      this.templatePath('testTemplate.txt'),
+      this.destinationPath('output.txt'), {
+        test: "Testing File Location"
+      }
     );
   }
 
-  install() {
-    this.installDependencies();
-  }
+  // install() {
+  //   this.installDependencies();
+  // }
 };
